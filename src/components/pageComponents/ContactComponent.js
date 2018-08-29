d164fd0949c08acabfadad3a31984316a727051c
zhileiz <zhileiz@seas.upenn.edu>
2018-08-29 13:54:01 +0800

    completed contact

--- src/components/pageComponents/ContactComponent.js
+++ src/components/pageComponents/ContactComponent.js
@@ -2,14 +2,60 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
+import './material/materialize.css'
+import './material/materialize.js'
+
+const FormDiv = styled.form`
+    .formInnerLine {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+        .nameLine {
+            width: 30%;
+        }
+        .emailLine {
+            width: 65%;
+        }
+    }
+    .submitter {
+        padding: 10px 0px;
+        border: none;
+        background: none;
+        font-size: 20px;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        font-weight: 500;
+        color: #666666;
+    }
+    .submitter:hover {
+        color: #26a69a;
+    }
+`;
+
 const ContactComponent = ({post}) => (
-    <form action="https://formspree.io/zheng.zhilei@hotmail.com" method="POST">
-        <label>Name:</label>
-        <input type="text" name="name"/> <br/>
-        <label>Email:</label>
-        <input type="email" name="_replyto"/><br/>
-        <input type="submit" value="Send"/>
-    </form>
+    <div>
+        <FormDiv action="https://formspree.io/zheng.zhilei@hotmail.com" method="POST">
+            <div className="formInnerLine">
+                <div className="nameLine">
+                    <label>Your Name:</label>
+                    <input type="text" name="name" required/> <br/>
+                </div>
+                <div className="emailLine">
+                    <label>Your Email:</label>
+                    <input type="email" name="_replyto" required/><br/>
+                </div>
+            </div>
+            <div>
+                <label>What is your topic today?</label>
+                <input type="text" name="_replyto" required/><br/>
+            </div>
+            <div>
+                <label>Your Message</label>
+                <input type="text" name="_replyto" required/><br/>
+            </div>
+            <input type="submit" value="Send" className="submitter"/>
+        </FormDiv>
+    </div>
 )
 
 export default ContactComponent
